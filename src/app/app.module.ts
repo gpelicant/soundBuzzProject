29236fe92a5ec9fbca66ec280da1387ea4887cbc
gpelicant <gpelicant@tradelab.fr>
2018-09-08 13:59:12 +0200

    scafolding angular material, angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+import { TestComponent } from './test/test.component';
+
+const routes: Routes = [
+  { path: 'test', component: TestComponent },
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true, useHash: true },
+    ),
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
